b053432f550627e573cd9ada6fc6be111ab7fac0
Nathan Ockerman <ockerman.ns@gmail.com>
2015-10-16 09:27:42 -0700

    Auth working (for the most part)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
+
+# Users can accept or reject edits, (both whole edit docs and specific lines)
+#
+# Revisions can be layered on the original document for the user to view.
+# 	- done as checkboxes along the top that when checked show that version of
+#    the document layered on top of all the other “checked” edits and revisions.
+#
+# Edits can be graded, and through that Users have an “Edit Score”/Ranking/
+
+
 Rails.application.routes.draw do
    root to: "welcome#index"
 
@@ -44,20 +54,24 @@ Rails.application.routes.draw do
    get "/edits/:id", to: "edits#show"
 end
 
-#| Prefix Verb  URI Pattern                     Controller#Action
-#|            root GET   /                               welcome#index
-#|           users GET   /users(.:format)                users#index
-#|         sign_up GET   /users/new(.:format)            users#new
-#|                 POST  /users(.:format)                users#create
-#|         profile GET   /users/:id(.:format)            users#show
-#|            user GET   /users/:id/edit(.:format)       users#edit
-#|                 PATCH /users/:id/edit(.:format)       users#update
-#|           login GET   /sessions/new(.:format)         sessions#new
-#|        sessions POST  /sessions(.:format)             sessions#create
-#|          logout GET   /sessions/destroy(.:format)     sessions#destroy
-#|     submissions GET   /submissions(.:format)          submissions#index
-#| submissions_new GET   /submissions/new(.:format)      submissions#new
-#|                 POST  /submissions(.:format)          submissions#create
-#|                 GET   /submissions/:id(.:format)      submissions#show
-#|                 GET   /submissions/:id/edit(.:format) submissions#edit
-#|                 PATCH /submissions/:id/edit(.:format) submission#update
+# |      Prefix  Verb   URI Pattern                         Controller#Action
+# |         root GET    /                                    welcome#index
+# |        users GET    /users(.:format)                     users#index
+# |      sign_up GET    /users/new(.:format)                 users#new
+# |              POST   /users(.:format)                     users#create
+# |      profile GET    /users/:id(.:format)                 users#show
+# |         user GET    /users/:id/edit(.:format)            users#edit
+# |              PATCH  /users/:id/edit(.:format)            users#update
+# |        login GET    /sessions/new(.:format)              sessions#new
+# |     sessions POST   /sessions(.:format)                  sessions#create
+# |       logout DELETE /sessions/destroy(.:format)          sessions#destroy
+# |  submissions GET    /submissions(.:format)               submissions#index
+# |              GET    /users/:id/submissions/new(.:format) submissions#new
+# |              POST   /submissions(.:format)               submissions#create
+# |              GET    /submissions/:id(.:format)           submissions#show
+# |              GET    /submissions/:id/edit(.:format)      submissions#edit
+# |              PATCH  /submissions/:id/edit(.:format)      submissions#update
+# |              DELETE /submissions/:id(.:format)           submissions#destroy
+# |              GET    /submissions/:id/edits/new(.:format) edits#new
+# |        edits POST   /edits(.:format)                     edits#create
+# |              GET    /edits/:id(.:format)                 edits#show
